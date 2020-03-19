cdca00a56db30861dbe604587b87c4d46ec794aa
Aidan Middleton <aidanemiddleton@gmail.com>
2020-03-19 13:06:11 -0700

    refactor

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,18 +5,16 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
-    return (<InterviewerListItem
-      id={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => {
-        props.setInterviewer(interviewer.id)
-      }}
-          
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
       />
-    )
-  })
+    );
+  });
 
   return (
     <section className="interviewers">
