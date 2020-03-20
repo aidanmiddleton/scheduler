8561108185e74f516f56fd8fbe8f1410e860742c
Aidan Middleton <aidanemiddleton@gmail.com>
2020-03-20 14:24:03 -0700

    added more components to appointments

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,12 @@ import InterviewerList from "../src/components/InterviewerList";
 import Appointment from "../src/components/Appointment/index";
 import Header from "../src/components/Appointment/Header";
 import Empty from "../src/components/Appointment/Empty";
+import Show from "../src/components/Appointment/Show";
+import Confirm from "../src/components/Appointment/Confirm";
+import Status from "../src/components/Appointment/Status";
+import Error from "../src/components/Appointment/Error";
+
+
 
 import "index.scss";
 
@@ -140,3 +146,21 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show 
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")} 
+    student="Lydia Miller"
+    interviewer={interviewer} 
+  />)
+  .add("Confirm", () => <Confirm
+    message='Delete the appointment?'
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Status", () => <Status
+    message='Deleting'
+  />)
+  .add("Error", () => <Error
+    message="Could not delete appointment."
+    onClose={action("onClose")}
+  />)
